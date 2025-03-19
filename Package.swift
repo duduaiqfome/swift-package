1e84d7915e6cf39a01c47c5fec1d4445ccef2e9a
Anka <runner@Mac-1742397668159.local>
2025-03-19 15:29:02 +0000

    Update to v2.2.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "pocKMP",
             url: "https://github.com/duduaiqfome/appKmp/releases/download/v1.13.0/Shared.xcframework.zip",
-            checksum: "93005bce3773c7992b7a34b23986404bef99bd551ba8a0ba30047bfbaee44439"
+            checksum: "5ea4ed5933d05ad8b89d4c3d60a6a55c37449d1f42c6bd4be79b32d363e17606"
         )
     ]
 )
